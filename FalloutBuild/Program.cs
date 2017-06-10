286a8af519c986dfc8b542d65b84f51a0bb804d6
David Mason <nerdcorerising@gmail.com>
2017-06-10 12:37:28 -0700

    inital commit

---
+++ FalloutBuild/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FalloutBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            IEnumerable<string> specials = new string[]{"strength", "perception", "endurance", "charisma", "intelligence", "agility", "luck"};
+            IEnumerable<int> levels = Enumerable.Range(1, 10);
+
+            Console.WriteLine("{");
+            Console.WriteLine("  \"special\": [ \"strength\", \"perception\", \"endurance\", \"charisma\", \"intelligence\", \"agility\", \"luck\" ],");
+            Console.WriteLine("  \"perks\": [");
+
+            foreach (string special in specials)
+            {
+                foreach (int level in levels)
+                {
+                    Console.WriteLine("    {");
+                    Console.WriteLine("      \"NIY\": [");
+                    Console.WriteLine("        {");
+                    Console.WriteLine($"          \"specialPreReq\": {{ \"{special}\": {level} }}");
+                    Console.WriteLine("        },");
+                    Console.WriteLine("        {");
+                    Console.WriteLine("          \"rank\": 1,");
+                    Console.WriteLine("          \"preReq\": { \"level\":  }");
+                    Console.WriteLine("        },");
+                    Console.WriteLine("        {");
+                    Console.WriteLine("          \"rank\": 2,");
+                    Console.WriteLine("          \"preReq\": { \"level\":  }");
+                    Console.WriteLine("        },");
+                    Console.WriteLine("        {");
+                    Console.WriteLine("          \"rank\": 3,");
+                    Console.WriteLine("          \"preReq\": { \"level\":  }");
+                    Console.WriteLine("        },");
+                    Console.WriteLine("        {");
+                    Console.WriteLine("          \"rank\": 4,");
+                    Console.WriteLine("          \"preReq\": { \"level\":  }");
+                    Console.WriteLine("        },");
+                    Console.WriteLine("        {");
+                    Console.WriteLine("          \"rank\": 5,");
+                    Console.WriteLine("          \"preReq\": { \"level\":  }");
+                    Console.WriteLine("        },");
+                    Console.WriteLine("      ]");
+                    Console.WriteLine("    },");
+                }
+            }
+
+            Console.WriteLine("  ]");
+            Console.WriteLine("}");
+        }
+    }
+}
